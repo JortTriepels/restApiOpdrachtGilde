53f5c51dae74fdfd04eac7162ee8903b7f9bfa1c
JortTriepels <503472@student.fontys.nl>
2025-09-11 12:38:50 +0200

    demo working

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+fetch("https://nominatim.openstreetmap.org/search?format=json&q=Neer", {
+    headers: {
+        'Accept': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log("API Result:", data);
+
+    const locatie = data[0];
+    const lat = locatie.lat;
+    const lon = locatie.lon;
+
+    
+    const map = L.map('map').setView([lat, lon], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    
+    L.marker([lat, lon])
+      .addTo(map)
+      .bindPopup("Neer")
+      .openPopup();
+})
+.catch(error => {
+    console.error("Fout bij API request:", error);
+});
