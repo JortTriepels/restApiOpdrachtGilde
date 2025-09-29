aaf6e6092736f5755a88fbb4c533c05bfcd11f9d
JortTriepels <503472@student.fontys.nl>
2025-09-29 12:37:12 +0200

    async function

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
 
 
-fetch("https://nominatim.openstreetmap.org/search?format=json&q=Gilde Opleidingen", {
-    headers: {
-        'Accept': 'application/json'
-    }
-})
-    .then(response => response.json())
-    .then(data => {
+
+async function doStuff() {
+    try {
+        const response = await fetch("https://nominatim.openstreetmap.org/search?format=json&q=Gilde Opleidingen", {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        const data = await response.json();
         console.log("API Result:", data);
 
-       
-        var wantedLocation = "marathonlaan"
+        let wantedLocation = "marathonlaan";
 
-        document.getElementById("enterButton").onclick = function(){
+        document.getElementById("enterButton").onclick = function () {
             wantedLocation = document.getElementById("textBox").value;
-            
-    
-        }
+        };
 
-
-       
-
-        //const locatie = data.find(item => item.display_name.includes(wantedLocation));
-        const locatie = data.find(item => item.display_name.toLowerCase().includes(wantedLocation.toLowerCase()));
-
-        
-
-
+        const locatie = data.find(item =>
+            item.display_name.toLowerCase().includes(wantedLocation.toLowerCase())
+        );
 
         if (locatie) {
-            //const locatie = data[6];
             const lat = locatie.lat;
             const lon = locatie.lon;
-
-
-
-
-         plaatsMarkerOpKaart(lat, lon, wantedLocation);
+            plaatsMarkerOpKaart(lat, lon, wantedLocation);
         }
-    })
-    .catch(error => {
-        console.error("Fout bij API request:", error);
-    });
 
-    function plaatsMarkerOpKaart(lat, lon, locationName) {
+    } catch (error) {
+        console.error("Fout bij API request:", error);
+    }
+}
+
+function plaatsMarkerOpKaart(lat, lon, locationName) {
     const map = L.map('map').setView([lat, lon], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -56,5 +46,5 @@ fetch("https://nominatim.openstreetmap.org/search?format=json&q=Gilde Opleidinge
         .openPopup();
 }
 
-    
-    
+// call async function
+doStuff();
